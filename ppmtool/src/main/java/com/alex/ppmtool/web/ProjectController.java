05d18c106e5d7867209d27a3fa5c39d51255da26
alexdavilwol <alexander.davil@hcl.com>
2021-10-28 16:25:14 -0400

    branch4 Project object validation part1

--- ppmtool/src/main/java/com/alex/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/alex/ppmtool/web/ProjectController.java
@@ -6,10 +6,13 @@ import com.alex.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,8 +25,13 @@ public class ProjectController {
     //map to create a new project and give an http service update thatll tell
     // whether or not creation was successful
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid ProjectObject", HttpStatus.BAD_REQUEST);
+        }
+
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 }
