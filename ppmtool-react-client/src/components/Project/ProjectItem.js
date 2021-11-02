78ae0571e039b4267ccb4e6c6a45fdedb2c05b07
alexdavilwol <alexander.davil@hcl.com>
2021-11-02 11:21:17 -0400

    branch13 Project and header componenets

---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ProjectItem extends Component {
+    render() {
+        return <h1>This is the Project Item</h1>;
+    }
+}
+
+ export default ProjectItem;
