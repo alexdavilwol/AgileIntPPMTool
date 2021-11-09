70d1f70029e5ff1aff7cc7de967e6c28239dce86
alexdavilwol <alexander.davil@hcl.com>
2021-11-09 10:08:55 -0500

    branch45 Routes to project board and add project task

--- ppmtool-react-client/src/actions/types.js
+++ ppmtool-react-client/src/actions/types.js
@@ -1,4 +1,15 @@
+//errors
 export const GET_ERRORS = "GET_ERRORS";
+//get list of projects to display on dashboard
+//get api to display projects from database
 export const GET_PROJECTS = "GET_PROJECTS";
+//Update the project
+//get a specific project by ID
 export const GET_PROJECT = "GET_PROJECT";
+//delete project
 export const DELETE_PROJECT = "DELETE_PROJECT";
+
+//TYPES FOR BACKLOG ACTIONS
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
