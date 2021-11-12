75b018802b1dbb76f8c228f2a2fa35579790e546
alexdavilwol <alexander.davil@hcl.com>
2021-11-12 11:58:40 -0500

    branch64 JWT Provider Prework - rewatch the lectures 0_0

--- ppmtool/src/main/java/com/alex/ppmtool/repositories/UserRepository.java
+++ ppmtool/src/main/java/com/alex/ppmtool/repositories/UserRepository.java
@@ -4,6 +4,15 @@ import com.alex.ppmtool.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+//import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+    User getById(Long id);
+
+    //what is Optional? could use this instead of getById
+    //Optional<User> findById(Long id);
+
 }
